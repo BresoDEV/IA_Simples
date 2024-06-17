897991d0054522601a36ccdc8b4afa8371adb9a7
BresoDEV <eduardobreso@gmail.com>
2024-06-17 00:36:45 -0300

    w

--- voids.js
+++ voids.js
@@ -1706,7 +1706,7 @@ function gerarImagem(p) {
          
         fetch('https://picsum.photos/200/200')
         .then(response =>{
-            
+            console.log(response.url)
 			//if (response.url.toLowerCase().includes('http')) {
 						//return 'Aqui esta: <br><img src=\"'+response.url+'" alt=\"\" style=\"width:50%\" srcset=\"\">'
 						return response.url
